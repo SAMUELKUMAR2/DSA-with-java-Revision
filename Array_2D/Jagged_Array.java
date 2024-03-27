e6e26ee7d57c30959237e36842076e20aab4f2ac
SAMUELKUMAR2 <146331228+SAMUELKUMAR2@users.noreply.github.com>
2024-03-27 17:02:08 +0530

    2D_Array
    Solving the problem like Transpose matrix, Search, Wave Print etc

---
+++ Array_2D/Jagged_Array.java
@@ -0,0 +1,12 @@
+package Array_2D;
+//that array who column in not fixed.
+public class Jagged_Array {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = new int[3][];
+		arr[0] = new int[2];
+		arr[1] = new int[6];
+		arr[2] = new int[1];
+
+	}
+}
